9378fc90755d3247ba1ad0d7fcc079279cc9dbde
joacalde <joacalde3@gmail.com>
2024-10-16 12:06:25 -0300

    Update Actividad.py

--- 02_code/server/Actividad.py
+++ 02_code/server/Actividad.py
@@ -1,19 +1,19 @@
-# Actividad.py
 from datetime import datetime
 from User import User
 
 class Actividad:
-    def __init__(self, user=None, orden="", detalle="", exito=False):
+    def __init__(self, id, user=None, orden="", detalle="", exito=False, marcaTiempo=None):
+        self.id = id
         self.user = user if user is not None else User()
         self.orden = orden
         self.detalle = detalle
         self.exito = exito
-        self.marcaTiempo = datetime.now()
+        self.marcaTiempo = marcaTiempo if marcaTiempo is not None else datetime.now()
 
     # Guardar en CSV (utiliza a sí mismo, a Actividad)
     def guardar_en_csv(self):
         # Placeholder para guardar en un archivo CSV
-        print(f"Guardando en CSV: Usuario={self.user.usuario}, Orden={self.orden}, Detalle={self.detalle}, Exito={self.exito}, Marca de Tiempo={self.marcaTiempo}")
+        print(f"Guardando en CSV: ID={self.id}, Usuario={self.user.usuario}, Orden={self.orden}, Detalle={self.detalle}, Exito={self.exito}, Marca de Tiempo={self.marcaTiempo}")
 
     # Tarea finalizada (dejado vacío según la consigna)
     def tarea_finalizada(self):
