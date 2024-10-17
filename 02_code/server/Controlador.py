d36151772ed493d853edbd67c018e5101875c40b
JuanMaDeMiguel <demiguelfunes.juan@uncuyo.edu.ar>
2024-10-16 23:30:29 -0300

    probando, conecta pero error por caracter

--- 02_code/server/Controlador.py
+++ 02_code/server/Controlador.py
@@ -1,5 +1,6 @@
 import serial
 import re
+import time
 
 class Controlador:
     def __init__(self, puerto, baudrate, time_out):
@@ -7,20 +8,30 @@ class Controlador:
         self.baudrate = baudrate
         self.time_out = time_out
         self.serial_connection = serial.Serial(port=self.puerto, baudrate=self.baudrate, timeout=self.time_out)
+        self.leer()
+        self.leer()
 
-    def leer(self):
-        if self.serial_connection.in_waiting > 0:
-            return self.serial_connection.readline().decode('utf-8').strip()
-        return ""
+    def leer(self, timeout=5):
+        start_time = time.time()
+        while True:
+            if self.serial_connection.in_waiting > 0:
+                data = self.serial_connection.readline().decode('utf-8').strip()
+                print(f"Leído: {data}")  # Debugging print statement
+                return data
+            if time.time() - start_time > timeout:
+                raise TimeoutError("Timeout while reading from serial port")
+            time.sleep(0.1)  # Esperar un poco antes de volver a verificar
 
     def escribir(self, s: str) -> None:
-        self.serial_connection.write((s + '\r\n').encode('utf-8'))
+        if self.serial_connection:
+            self.serial_connection.write((s + '\r\n').encode('utf-8'))
+
 
     def inicializacion(self):
-        self.escribir("M114\r\n")
-        respuesta = self.leer()
+        self.escribir("M114\r")
         # Parse the response
-        modo_coordenadas = 0 if "ABSOLUTE MODE" in respuesta else 1
+        modo_coordenadas = 0 if "ABSOLUTE MODE" in self.leer() else 1
+        respuesta = self.leer()
         coordenadas = re.findall(r'X:(-?\d+\.\d+)\sY:(-?\d+\.\d+)\sZ:(-?\d+\.\d+)\sE:(-?\d+\.\d+)', respuesta)
         x, y, z, e = map(float, coordenadas[0])
         e_bool = e != 0.0
--- 02_code/server/main.py
+++ 02_code/server/main.py
@@ -5,10 +5,11 @@ from InterfazServidor import InterfazServidor
 
 # Main del programa
 if __name__ == "__main__":
-    controlador = Controlador(puerto="COM3", baudrate=9600, time_out=1)
-    server = Server(modo_coordenadas=False, modo_trabajo=True, controlador=controlador)
+    controlador = Controlador(puerto="COM6", baudrate=115200, time_out=1)
+    server = Server(modo_trabajo=True, controlador=controlador)
     controlador_servidor = ControladorServidor(servidor=server)
     interfaz = InterfazServidor(controlador_servidor=controlador_servidor)
+
 
     # Ejemplo: se inicia con el menú de usuario
     interfaz.mostrar_menu_usuario()
